f88024946bbc7d1a8d8a78bf424aa12628812584
aaronvanzyl <aaron@vanzyl.org>
2020-06-16 13:13:29 -0700

    Add login dialog outline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
 
@@ -23,7 +24,8 @@ import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 
-import { AppRoutingModule } from './app-routing/app-routing.module'
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component'
 
 @NgModule({
   declarations: [
@@ -35,6 +37,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module'
     HomeComponent,
     AboutComponent,
     ContactComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -46,8 +49,11 @@ import { AppRoutingModule } from './app-routing/app-routing.module'
     MatCardModule,
     MatButtonModule,
     AppRoutingModule,
+    MatDialogModule
   ],
   providers: [DishService, PromotionService, LeaderService],
+  entryComponents: [LoginComponent],
   bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
